794b55daef3ee5d31630eb2d6adf38ed73c307bb
Alek Parol <alekparol92@gmail.com>
2018-12-17 13:08:00 +0100

    Adding GoToCategory() Method
    This method will navigate webdriver directly to the category url and return expected driver's url address.

--- Category.cs
+++ Category.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenQA.Selenium;
 
 namespace WebScraper
 {
     public class Category
     {
+
+        /**
+         * TODO: Test Category URL.      
+         * TODO: Move all functions which are used in differents modules to the one utility component. OR use changeName(), changeLocalation() etc. as a method of page class, 
+         * as those methods do the same job - baseURL + / + component_first + / component_second + /.         
+         */
 
         private string baseURL = "https://www.olx.pl/";
         private string productCategory;
@@ -71,25 +78,32 @@ namespace WebScraper
 
         }
 
-        public Category(string productCategory)
+        public Category (string productCategory)
         {
 
             this.productCategory = productCategory;
-
             string[] categoryAuxilliary = productCategory.Split("»");
 
             mainCategory = categoryAuxilliary[0].Trim();
             subCategory = categoryAuxilliary[1].Trim();
 
-            categoryURL = baseURL + "/" + mainCategory.ToLower() + "/" + subCategory.ToLower().Replace(" ", "-");
+            categoryURL = baseURL + "/" + mainCategory.ToLower().Replace(" ", "-") + "/" + subCategory.ToLower().Replace(" ", "-") + "/";
 
         }
 
-        public void DisplayCategory()
+        public void DisplayCategory ()
         {
 
             Console.WriteLine("Product main category is: {0}", mainCategory);
             Console.WriteLine("Product subcategory is: {0}", subCategory);
+
+        }
+
+        public string GoToCategory (IWebDriver driver)
+        {
+
+            driver.Navigate().GoToUrl(categoryURL);
+            return categoryURL;
 
         }
 
